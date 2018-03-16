4c58db518e4cb99e3fdf049210ef003c806853f8
wararyo <wararyo@gmail.com>
2018-03-16 22:42:42 +0900

    デバイス名戻した

--- HID-HorizontalScrollMouse/usbconfig.h
+++ HID-HorizontalScrollMouse/usbconfig.h
@@ -238,8 +238,8 @@ section at the end of this file).
 #define USB_CFG_DEVICE_VERSION  0x00, 0x01
 /* Version number of the device: Minor number first, then major number.
  */
-#define USB_CFG_VENDOR_NAME     'w', 'a', 'r', 'a', 'r', 'y', 'o', '.', 'c', 'o', 'm'
-#define USB_CFG_VENDOR_NAME_LEN 11
+#define USB_CFG_VENDOR_NAME     'o', 'b', 'd', 'e', 'v', '.', 'a', 't'
+#define USB_CFG_VENDOR_NAME_LEN 8
 /* These two values define the vendor name returned by the USB device. The name
  * must be given as a list of characters under single quotes. The characters
  * are interpreted as Unicode (UTF-16) entities.
@@ -248,8 +248,8 @@ section at the end of this file).
  * obdev's free shared VID/PID pair. See the file USB-IDs-for-free.txt for
  * details.
  */
-#define USB_CFG_DEVICE_NAME     'H', 'W', 'h', 'e', 'e', 'l'
-#define USB_CFG_DEVICE_NAME_LEN 6
+#define USB_CFG_DEVICE_NAME     'M', 'o', 'u', 's', 'e'
+#define USB_CFG_DEVICE_NAME_LEN 5
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USB-IDs-for-free.txt before you assign a name if
  * you use a shared VID/PID.
